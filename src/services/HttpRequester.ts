082c6c5cec8feb7da63a2eaf74345aded8b1d173
Baptiste LINEL <AJ1034869@tdf.hubtotal.net>
2021-09-29 16:22:28 +0200

    Update gitignore

--- src/services/HttpRequester.ts
+++ src/services/HttpRequester.ts
@@ -5,8 +5,7 @@ const apiSpotifyUrl = "https://api.spotify.com/v1";
 // Client id = ba89e08371ff47e5a272b2796c66d628
 // Client secret = b601ea2dc98e488288a503542327c35c
 
-const authApiToken =
-  "BQAmmlo2w31ax9CFJKdNaZpom2xSWIdjpHCgpoPmwjTQlLK5VisA6TSIbLLE1SZ2r367Zs4OPDmk8Fqgj54sCOqapPiu3TvGEZq3Qg_RnqpsMQ5comd5415-j4Bgk20VVlINTf8TKWKWyUt0dZ9oNZjYxSH6PkFyyU4WTMCT8YlBhYpo";
+const authApiToken = "Your Spotify Token here";
 
 export const httpGet = (endpoint: string) => {
   return request
